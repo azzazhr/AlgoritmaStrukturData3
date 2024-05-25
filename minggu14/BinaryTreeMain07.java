753581db8e2829675c8ebf816b0bff6a9c6ab647
azzazhr <azzahraqina70@gmail.com>
2024-05-25 19:12:24 +0700

    add Main BinaryTree(percobaan1)

---
+++ minggu14/BinaryTreeMain07.java
@@ -0,0 +1,34 @@
+package minggu14;
+
+public class BinaryTreeMain07 {
+    public static void main(String[] args) {
+
+        BinaryTree07 bt = new BinaryTree07();
+        bt.add(6);
+        bt.add(4);
+        bt.add(8);
+        bt.add(3);
+        bt.add(5);
+        bt.add(7);
+        bt.add(9);
+        bt.add(10);
+        bt.add(15);
+        System.out.print("PreOrder Traversal : ");
+        bt.traversePreOrder(bt.root);
+        System.out.println("");
+        System.out.print("InPorder Travesal : ");
+        bt.traverseInOrder(bt.root);
+        System.out.println("");
+        System.out.print("PostOrder Travesal : ");
+        bt.traversePostOrder(bt.root);
+        System.out.println("");
+        System.out.println("Find Node : " + bt.find(5));
+        System.out.println("Delete Node 8");
+        bt.delete(8);
+        System.out.println("");
+        System.out.print("PreOrder Travesal : ");
+        bt.traversePreOrder(bt.root);
+        System.out.println("");
+    }
+
+}
