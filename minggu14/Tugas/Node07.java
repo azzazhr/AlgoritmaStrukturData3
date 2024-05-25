f1ac4eb5d3707429d1a940832f43934e8dc1679e
azzazhr <azzahraqina70@gmail.com>
2024-05-25 20:08:38 +0700

    add Class Node(Tugas praktikum)

---
+++ minggu14/Tugas/Node07.java
@@ -0,0 +1,12 @@
+package minggu14.Tugas;
+
+public class Node07 {
+    int data;
+    Node07 left, right;
+
+    public Node07(int data) {
+        this.data = data;
+        left = right = null;
+    }
+
+}
