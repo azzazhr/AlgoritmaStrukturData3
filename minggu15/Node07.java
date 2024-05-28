86ef0712fcba07645d97fcdad04ffc7ef8d53224
azzazhr <azzahraqina70@gmail.com>
2024-05-28 21:38:51 +0700

    add Class Node

---
+++ minggu15/Node07.java
@@ -0,0 +1,14 @@
+package minggu15;
+
+public class Node07 {
+    int data;
+    Node07 prev, next;
+    int jarak;
+
+    Node07(Node07 prev, int data, int jarak, Node07 next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+        this.jarak = jarak;
+    }
+}
