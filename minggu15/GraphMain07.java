58877f01ca41b52bfb8ef5718a6b885b706ab933
azzazhr <azzahraqina70@gmail.com>
2024-05-29 16:19:57 +0700

    add Main Graph(modify percobaan1 no5

--- minggu15/GraphMain07.java
+++ minggu15/GraphMain07.java
@@ -1,7 +1,11 @@
 package minggu15;
 
+import java.util.Scanner;
+
 public class GraphMain07 {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
         Graph07 gedung = new Graph07(6);
         gedung.addEdge(0, 1, 50);
         gedung.addEdge(0, 2, 100);
@@ -14,6 +18,26 @@ public class GraphMain07 {
         System.out.println();
         gedung.removeEdge(1, 3);
         gedung.printGraph();
+        System.out.println();
 
+        while (true) {
+            System.out.print("Masukkan gedung asal: ");
+            int start = scanner.nextInt();
+            if (start == -1)
+                break;
+
+            System.out.print("Masukkan gedung tujuan: ");
+            int end = scanner.nextInt();
+
+            boolean pathExists = gedung.isPathExists(start, end);
+            if (pathExists) {
+                System.out.println("Gedung " + (char) ('A' + start) + " dan " + (char) ('A' + end) + " bertetangga");
+            } else {
+                System.out.println(
+                        "Gedung " + (char) ('A' + start) + " dan " + (char) ('A' + end) + " tidak bertetangga");
+            }
+        }
+
+        scanner.close();
     }
 }
